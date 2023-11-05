431f20e962b2a5bc8fa40f665c7039d493ff9892
qwert <qwert@DESKTOP-P1C90IK>
2023-11-05 20:08:05 -0300

    Carga inicial

--- src/ClaseMath/EjemploClaseMath.java
+++ src/ClaseMath/EjemploClaseMath.java
@@ -8,7 +8,18 @@ public class EjemploClaseMath {
         int absoluto = Math.abs(-3);
         System.out.println("absoluto = " + absoluto);
         
-        absoluto
+        absoluto = Math.abs(3);
+        System.out.println("absoluto = " + absoluto);
+        
+        double max = Math.max(3.5,1.2);
+        System.out.println("max = " + max);
+        
+        double min = Math.min(3.5,1.2);
+        System.out.println("min = " + min);
+        
+        double techo = Math.ceil(3.5);
+        System.out.println("techo = " + techo);
+        
     }
     
 }
