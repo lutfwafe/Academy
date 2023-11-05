eacdb54412551f3feffec8ca812b66a40af3bbfc
qwert <qwert@DESKTOP-P1C90IK>
2023-11-05 19:28:57 -0300

    Carga inicial

---
+++ src/ClaseMath/EjemploClaseMath.java
@@ -0,0 +1,14 @@
+
+package ClaseMath;
+
+
+public class EjemploClaseMath {
+    public static void main(String[] args) {
+        
+        int absoluto = Math.abs(-3);
+        System.out.println("absoluto = " + absoluto);
+        
+        absoluto
+    }
+    
+}
